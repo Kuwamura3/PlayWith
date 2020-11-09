c52fda3bcc63a85b59e7dfdac09bbab548794463
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2020-11-09 07:27:35 +0000

    [Finish] 新規登録、ログイン機能／ヘッダー記述忘れ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,25 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable,
-				 :recoverable, :rememberable, :validatable
+				 :recoverable, :rememberable, :validatable, :authentication_keys => [:name]
 
 	has_many :relationships
 	has_many :users_comments
 	has_many :notifications
 	has_many :users_games
 	has_many :posts
+
+	# emailカラムを使用しない
+	def email_required?
+		false
+	end
+
+	def email_changed?
+		false
+	end
+
+	def will_save_change_to_email?
+		false
+	end
 
 end
