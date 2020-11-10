1d5d282c85c1b78cfa4301c93ad64e8af8cafd95
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2020-11-10 08:23:58 +0000

    [Add] プロフィールイメージ、フォローボタン(見た目)、フォロー一覧リンク、編集画面リンク

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,23 +1,23 @@
 class Public::UsersController < ApplicationController
 
-  def search
-  end
+	def search
+	end
 
-  def game
-  end
+	def game
+	end
 
-  def index
-    @users = User.page(params[:page]).per(PER)
-  end
+	def index
+		@users = User.page(params[:page]).per(PER)
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def show
-    @user = User.find(params[:id])
-  end
+	def show
+		@user = User.find(params[:id])
+	end
 
-  def update
-  end
+	def update
+	end
 
 end
