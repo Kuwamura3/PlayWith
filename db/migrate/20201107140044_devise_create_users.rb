9458456e2b5a42fb15cef7411389510f0d70d81d
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2020-11-14 08:52:32 +0000

    [Fix] マイグレーションファイル

--- db/migrate/20201107140044_devise_create_users.rb
+++ db/migrate/20201107140044_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
-      t.text :introduction, default: "よろしくお願いします"
+      t.text :introduction
       t.string :image
       t.integer :voice, default: 0 #未設定
       t.string :twitter
