e496c205f28c27e42602f50928462c719c863a17
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2020-11-15 09:26:05 +0000

    [Add] 投稿機能、見た目まで

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
 
 	belongs_to :user
 
+	enum kind: { 新規: 0, 遊びたい: 1}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_15_082150) do
+ActiveRecord::Schema.define(version: 2020_11_15_091309) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 2020_11_15_082150) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "type"
+    t.integer "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "game_id"
   end
 
   create_table "relationships", force: :cascade do |t|
