2d5b80551b08f2ed7bb4beac1ae049a28b86177c
EC2 Default User <ec2-user@ip-172-31-23-32.ap-northeast-1.compute.internal>
2020-11-17 09:40:16 +0000

    [Finish] エラーメッセージの日本語化(rails-i18n)、CSS

--- app/controllers/public/users_comments_controller.rb
+++ app/controllers/public/users_comments_controller.rb
@@ -9,9 +9,11 @@ class Public::UsersCommentsController < ApplicationController
 			redirect_to user_path(params[:id])
 		else
 			flash.now[:alert] = "コメントの内容を入力して下さい"
+			@users = User.all
 			@user = User.find(params[:id])
 			@users_games = current_user.playings.order(:game_id)
 			@users_comments = UsersComment.where(commented_id: params[:id]).order(id: "DESC") #降順
+			@notifications = Notification.where(user_id: @user.id).order(id: "DESC")
 			render template: "public/users/show"
 		end
 	end
