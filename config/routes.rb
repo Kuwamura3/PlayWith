f1052f7b8c0bb40ed98e5d2f412debdf398b70a3
EC2 Default User <ec2-user@ip-172-31-64-163.ec2.internal>
2020-12-03 08:21:36 +0000

    [Start] 管理者側、下準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :admins, controllers: {
+		sessions:      'users/sessions',
+		passwords:     'users/passwords',
+	}
+
 	devise_for :users, controllers: {
 		sessions:      'users/sessions',
 		passwords:     'users/passwords',
