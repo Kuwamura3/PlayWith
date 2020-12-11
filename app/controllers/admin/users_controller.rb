225760e6c41b9a44bd5355de6423d379959bb80a
EC2 Default User <ec2-user@ip-172-31-64-163.ec2.internal>
2020-12-11 02:55:52 +0000

    [Fix] 管理者側、プロフィールのテーブル周辺修正、テンプレート化

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,19 +1,19 @@
 class Admin::UsersController < ApplicationController
 	before_action :authenticate_admin!, only: [:index, :edit, :show]
 
-  def index
+	def index
 		@users = User.page(params[:page]).per(PER)
-  end
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def show
+	def show
 		@user = User.find(params[:id])
 		@users_games = @user.playings.order(:game_id)
 		@users_comments = UsersComment.where(commented_id: params[:id]).order(id: "DESC") #降順
-  end
+	end
 
-  def update
-  end
+	def update
+	end
 end
