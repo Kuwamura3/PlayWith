3b8496320085ea7973a7cc140368e3f06e4cbff0
EC2 Default User <ec2-user@ip-172-31-64-163.ec2.internal>
2020-12-14 07:06:12 +0000

    [Add] 管理者側、ゲーム新規登録、関連リンク調整

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -6,8 +6,27 @@ class Admin::GamesController < ApplicationController
   end
 
   def create
+    @game = Game.new(game_params)
+    unless Game.find_by(title: @game.title)
+      if @game.save
+        flash[:notice] = "ゲームを新規登録しました"
+        redirect_to games_path
+      else
+        render :new
+      end
+    else
+      flash.now[:alert] = "そのゲームは登録済です"
+      render :new
+    end
   end
 
   def new
   end
+  
+  private
+
+  def game_params
+    params.permit(:title)
+  end
+
 end
